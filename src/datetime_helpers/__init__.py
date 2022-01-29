84452971eff9b769e375848d0c093cc4b512077e
Rikhil <23627977+rikhilrai@users.noreply.github.com>
2022-01-29 19:03:39 +0000

    .

--- src/datetime_helpers/__init__.py
+++ src/datetime_helpers/__init__.py
@@ -2,7 +2,7 @@
 try:
     from importlib.metadata import version
 except ImportError:
-    from importlib_metadata import version
+    from importlib_metadata import version  # type: ignore[no-redef]
 
 __version__: str = version(__name__)
 
